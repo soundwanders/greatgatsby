4ce38f80f5bb20312bbcaeaf42654cce13d8d2e4
soundwanders <soundwanders@gmail.com>
2021-09-16 23:34:52 -0400

    Projects placeholder code

--- src/components/section/Projects/index.jsx
+++ src/components/section/Projects/index.jsx
@@ -1,90 +1,13 @@
 import React from "react";
-import { useStaticQuery, graphql } from "gatsby";
-import { Card, TitleWrap } from "components/common";
-import Star from "components/common/Symbols/Star";
-import Fork from "components/common/Symbols/Fork";
-import { ProjectContainer, Wrapper, Grid, Item, Content, Stats, Languages, } from "./style";
+import { ProjectContainer, Grid } from "./style";
 
 export const Projects = () => {
-  const {
-    github: {
-      viewer: {
-        repositories: { edges },
-      },
-    },
-  } = useStaticQuery(
-    graphql`
-      {
-        github {
-          viewer {
-            repositories(
-              first: 8
-              orderBy: { field: STARGAZERS, direction: DESC }
-            ) {
-              edges {
-                node {
-                  id
-                  name
-                  url
-                  description
-                  stargazers {
-                    totalCount
-                  }
-                  forkCount
-                  languages(first: 3) {
-                    nodes {
-                      id
-                      name
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    `
-  );
   return (
-    <Wrapper as={ProjectContainer} id="projects">
-      <h2>Projects</h2>
+    <ProjectContainer id="projects">
       <Grid>
-        {edges.map(({ node }) => (
-          <Item
-            key={node.id}
-            as="a"
-            href={node.url}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Card>
-              <Content>
-                <h4>{node.name}</h4>
-                <p>{node.description}</p>
-              </Content>
-              <TitleWrap>
-                <Stats>
-                  <div>
-                    <Star color="#f8f8f8" />
-                    <span>{node.stargazers.totalCount}</span>
-                  </div>
-                  <div>
-                    <Fork color="#f5f5f5" />
-                    <span>{node.forkCount}</span>
-                  </div>
-                </Stats>
-                <Stats>
-                  <Languages>
-                    {node.languages.nodes.map(({ id, name }) => (
-                      <span key={id}>{name}</span>
-                    ))}
-                  </Languages>
-                </Stats>
-              </TitleWrap>
-            </Card>
-          </Item>
-        ))}
+        <h1>Projects</h1>
+        <p> These are my projects</p>
       </Grid>
-    </Wrapper>
+    </ProjectContainer>
   );
 };
