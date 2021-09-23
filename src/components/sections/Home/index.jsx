28706e6319c679cbf39388993dec14d02b19bfcf
soundwanders <soundwanders@gmail.com>
2021-09-22 20:34:16 -0400

    Added svg pattern to Home section bg. Remove padding from modal

--- src/components/sections/Home/index.jsx
+++ src/components/sections/Home/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Seo } from 'components/common';
-import { HomeContainer, User, Subtitle } from './style';
+import { HomeContainer, Decor, User, Subtitle } from './style';
 
 export const HomePage = () => {
   return (
     <HomeContainer id="home">
       <Seo title="Home" />
+      <Decor />
       <User> Jared Coletta </User>
       <Subtitle> &#60; Web Developer &#47;&nbsp;&#62;</Subtitle>
     </HomeContainer>
--- src/components/sections/Home/style.js
+++ src/components/sections/Home/style.js
@@ -12,16 +12,31 @@ export const HomeContainer = styled.div`
   height: 99vh;
   overflow-x: hidden;
   background: #f8f8f8;
-  background-image: linear-gradient(
-    to bottom,
-    #f8f8f8,
-    #f7f7f7,
-    #f5f5f5,
-    #f4f4f4,
-    #f2f2f2,
-  );
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 2000 1500'%3E%3Cdefs%3E%3Crect stroke='%23ffffff' stroke-width='0.6' width='1' height='1' id='s'/%3E%3Cpattern id='a' width='3' height='3' patternUnits='userSpaceOnUse' patternTransform='scale(50) translate(-980 -735)'%3E%3Cuse fill='%23fcfcfc' href='%23s' y='2'/%3E%3Cuse fill='%23fcfcfc' href='%23s' x='1' y='2'/%3E%3Cuse fill='%23fafafa' href='%23s' x='2' y='2'/%3E%3Cuse fill='%23fafafa' href='%23s'/%3E%3Cuse fill='%23f7f7f7' href='%23s' x='2'/%3E%3Cuse fill='%23f7f7f7' href='%23s' x='1' y='1'/%3E%3C/pattern%3E%3Cpattern id='b' width='7' height='11' patternUnits='userSpaceOnUse' patternTransform='scale(50) translate(-980 -735)'%3E%3Cg fill='%23f5f5f5'%3E%3Cuse href='%23s'/%3E%3Cuse href='%23s' y='5' /%3E%3Cuse href='%23s' x='1' y='10'/%3E%3Cuse href='%23s' x='2' y='1'/%3E%3Cuse href='%23s' x='2' y='4'/%3E%3Cuse href='%23s' x='3' y='8'/%3E%3Cuse href='%23s' x='4' y='3'/%3E%3Cuse href='%23s' x='4' y='7'/%3E%3Cuse href='%23s' x='5' y='2'/%3E%3Cuse href='%23s' x='5' y='6'/%3E%3Cuse href='%23s' x='6' y='9'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='h' width='5' height='13' patternUnits='userSpaceOnUse' patternTransform='scale(50) translate(-980 -735)'%3E%3Cg fill='%23f5f5f5'%3E%3Cuse href='%23s' y='5'/%3E%3Cuse href='%23s' y='8'/%3E%3Cuse href='%23s' x='1' y='1'/%3E%3Cuse href='%23s' x='1' y='9'/%3E%3Cuse href='%23s' x='1' y='12'/%3E%3Cuse href='%23s' x='2'/%3E%3Cuse href='%23s' x='2' y='4'/%3E%3Cuse href='%23s' x='3' y='2'/%3E%3Cuse href='%23s' x='3' y='6'/%3E%3Cuse href='%23s' x='3' y='11'/%3E%3Cuse href='%23s' x='4' y='3'/%3E%3Cuse href='%23s' x='4' y='7'/%3E%3Cuse href='%23s' x='4' y='10'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='c' width='17' height='13' patternUnits='userSpaceOnUse' patternTransform='scale(50) translate(-980 -735)'%3E%3Cg fill='%23f2f2f2'%3E%3Cuse href='%23s' y='11'/%3E%3Cuse href='%23s' x='2' y='9'/%3E%3Cuse href='%23s' x='5' y='12'/%3E%3Cuse href='%23s' x='9' y='4'/%3E%3Cuse href='%23s' x='12' y='1'/%3E%3Cuse href='%23s' x='16' y='6'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='d' width='19' height='17' patternUnits='userSpaceOnUse' patternTransform='scale(50) translate(-980 -735)'%3E%3Cg fill='%23ffffff'%3E%3Cuse href='%23s' y='9'/%3E%3Cuse href='%23s' x='16' y='5'/%3E%3Cuse href='%23s' x='14' y='2'/%3E%3Cuse href='%23s' x='11' y='11'/%3E%3Cuse href='%23s' x='6' y='14'/%3E%3C/g%3E%3Cg fill='%23efefef'%3E%3Cuse href='%23s' x='3' y='13'/%3E%3Cuse href='%23s' x='9' y='7'/%3E%3Cuse href='%23s' x='13' y='10'/%3E%3Cuse href='%23s' x='15' y='4'/%3E%3Cuse href='%23s' x='18' y='1'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='e' width='47' height='53' patternUnits='userSpaceOnUse' patternTransform='scale(50) translate(-980 -735)'%3E%3Cg fill='%23F60'%3E%3Cuse href='%23s' x='2' y='5'/%3E%3Cuse href='%23s' x='16' y='38'/%3E%3Cuse href='%23s' x='46' y='42'/%3E%3Cuse href='%23s' x='29' y='20'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='f' width='59' height='71' patternUnits='userSpaceOnUse' patternTransform='scale(50) translate(-980 -735)'%3E%3Cg fill='%23F60'%3E%3Cuse href='%23s' x='33' y='13'/%3E%3Cuse href='%23s' x='27' y='54'/%3E%3Cuse href='%23s' x='55' y='55'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='g' width='139' height='97' patternUnits='userSpaceOnUse' patternTransform='scale(50) translate(-980 -735)'%3E%3Cg fill='%23F60'%3E%3Cuse href='%23s' x='11' y='8'/%3E%3Cuse href='%23s' x='51' y='13'/%3E%3Cuse href='%23s' x='17' y='73'/%3E%3Cuse href='%23s' x='99' y='57'/%3E%3C/g%3E%3C/pattern%3E%3C/defs%3E%3Crect fill='url(%23a)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23b)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23h)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23c)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23d)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23e)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23f)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23g)' width='100%25' height='100%25'/%3E%3C/svg%3E");
+  background-attachment: fixed;
+  background-size: cover;
   z-index: -4;
 `;
+
+export const Decor = styled.div`
+  width: 30vw;
+  white-space: no-wrap;
+  padding: 0;
+  margin: 0;
+  background-color: transparent; 
+  fill: #f8f8f8; 
+  background-size: 623px 623px; 
+  background-repeat: repeat; 
+  background-image: url("data:image/svg+xml,%3Csvg id='Layer_1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 500 500'%3E%3Cstyle%3E.st0%7Bfill:rgba(255,255,255,1)%7D%3C/style%3E%3Cpath class='st0' d='M125 250v125l125 125V375zM250 125L125 0v125l125 125zM250 250v125l125-125V125zM500 250V125L375 0v125zM500 500V375L375 250v125zM125 500V375L0 500zM375 500V375L250 500zM250 0v125L375 0zM0 125L125 0H0zM0 250v125l125-125V125z'/%3E%3C/svg%3E")
+                    linear-gradient(
+                      to bottom,
+                      #f8f8f8,
+                      #f7f7f7,
+                      #f5f5f5,
+                      #f4f4f4,
+                      #f2f2f2,
+                    );
+`
 
 export const User = styled.h1`
   display: inherit;
@@ -30,9 +45,9 @@ export const User = styled.h1`
   padding: 0;
   font-size: 10vmax;
   background: #5d2ad3;
-  background: -webkit-linear-gradient(to right, #0461ed 30%, #ff662e 100%);
-  background: -moz-linear-gradient(to right, #0461ed 30%, #ff662e 100%);
-  background: linear-gradient(to right, #0461ed 30%, #ff662e 100%);
+  background: -webkit-linear-gradient(to right, #333399 15%, #ff662e 100%);
+  background: -moz-linear-gradient(to right, #333399 15%, #ff662e 100%);
+  background: linear-gradient(to right, #333399 15%, #ff662e 100%);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
   @media (max-width: 600px) {
@@ -47,10 +62,22 @@ export const Subtitle = styled.h1`
   margin: 0 auto;
   white-space: nowrap;
   font-size: 3vmax;
-  color: #0461ed;
+  color: #333399;
   font-weight: 600;
   padding-top: 2rem;
   @media (max-width: 600px) {
     transform: translateY(-3rem);
   }
 `;
+
+
+/** 
+ *   background-image: linear-gradient(
+    to bottom,
+    #f8f8f8,
+    #f7f7f7,
+    #f5f5f5,
+    #f4f4f4,
+    #f2f2f2,
+  );
+ */
