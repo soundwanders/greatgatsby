beb7bb1d87e2326b7eca12419248387ab529d40d
soundwanders <soundwanders@gmail.com>
2021-09-22 20:36:12 -0400

    Linear gradient added to bg svg, remove useless Decor component for now

--- src/components/sections/Home/index.jsx
+++ src/components/sections/Home/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Seo } from 'components/common';
-import { HomeContainer, Decor, User, Subtitle } from './style';
+import { HomeContainer, User, Subtitle } from './style';
 
 export const HomePage = () => {
   return (
     <HomeContainer id="home">
       <Seo title="Home" />
-      <Decor />
       <User> Jared Coletta </User>
       <Subtitle> &#60; Web Developer &#47;&nbsp;&#62;</Subtitle>
     </HomeContainer>
