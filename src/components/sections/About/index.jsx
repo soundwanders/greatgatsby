64373d01c2543c327431d8f74b4b8628fd02cd91
soundwanders <soundwanders@gmail.com>
2021-09-27 23:44:57 -0400

    Update index.jsx

--- src/components/sections/About/index.jsx
+++ src/components/sections/About/index.jsx
@@ -13,11 +13,15 @@ export const About = () => {
   return (
     <Container id="about">
       <Title>Hey there! 👋 </Title>
-      <Subtitle>Let's Bring Your Big Idea to Life.</Subtitle>
+      <Subtitle>
+        A large part of why I fell in love with web development is that it offers me an artistic, creative outlet that also challenges me to constantly improve. There is never a shortage of problems to solve, ideas to explore and knowledge to gain.
+        I am not afraid to fail (maybe even fall flat on my face, sometimes) because I believe that through friction comes growth.
+        While it feels like my journey as a developer is only beginning, I have found myself gravitating towards a few core concepts when I am working on any project...
+      </Subtitle>
       <Content>
-        <DetailsTop>Creative Solutions</DetailsTop>
-        <DetailsMid>Passion & Creativity</DetailsMid>
-        <DetailsBottom>Open Communications</DetailsBottom>
+        <DetailsTop>Clean Code</DetailsTop>
+        <DetailsMid>Creative Solutions </DetailsMid>
+        <DetailsBottom>Responsive Design</DetailsBottom>
       </Content>
     </Container>
   );
