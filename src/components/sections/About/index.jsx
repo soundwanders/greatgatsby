6b9f5d77a4dd101ed5f4d20f0993ab6fe7277b9d
soundwanders <soundwanders@gmail.com>
2021-09-28 23:34:01 -0400

    Update index.jsx
    Remove line breaks as they are unnecessary now that elements have been styled with proper padding.

--- src/components/sections/About/index.jsx
+++ src/components/sections/About/index.jsx
@@ -18,18 +18,18 @@ export const About = () => {
         provides. It offers me the perfect opportunity to improve my artistic
         and technical skills.
       </Subtitle>
-      <br />
+      
       <Subtitle>
         There is never a shortage of problems to solve, ideas to explore,
         knowledge to absorb, and that's a beautiful thing.
       </Subtitle>
-      <br />
+      
       <Subtitle>
         I've stumbled a couple of times along the way (maybe even fell flat on
         my face, sometimes). I have learned to embrace the adversity and enjoy
         the growth that comes from it.
       </Subtitle>
-      <br />
+      
       <Subtitle>
         While my journey as a Developer is only beginning, I have noticed that I
         gravitate towards a few core concepts whenever I work on a new
