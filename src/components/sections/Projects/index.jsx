40ec6cc4ff796e2eb11ce5926d7a24862bb8f38b
soundwanders <soundwanders@gmail.com>
2021-10-14 21:42:52 -0400

    React modals. Multiple modals in same component

--- src/components/sections/Projects/index.jsx
+++ src/components/sections/Projects/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Modal from 'react-modal';
 import {
   ProjectContainer,
   Wrapper,
@@ -7,127 +8,221 @@ import {
   Item,
   ModalWrapper,
 } from './style';
-import { Modal } from 'components/common';
 
 export class Projects extends Component {
-  constructor() {
-    super();
-    this.state = { show: false };
-    this.showModal = this.showModal.bind(this);
-    this.hideModal = this.hideModal.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      handleOpenModal: false,
+      activeModal: '',
+    };
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
   }
 
-  showModal = () => {
-    this.setState({ show: true });
-  };
+  handleOpenModal(val) {
+    this.setState({ activeModal: val });
+    this.setState({ handleOpenModal: true });
+  }
 
-  hideModal = () => {
-    this.setState({ show: false });
-  };
+  handleCloseModal() {
+    this.setState({ handleOpenModal: false });
+    this.setState({ handleOpenModal: '' });
+  }
 
   render() {
     return (
-      <Wrapper>
-        <ProjectContainer id="projects">
-          <Grid>
-            <Card>
-              <Item>
-                <Modal
-                  show={this.state.show}
-                  handleClose={this.hideModal}
-                ></Modal>
-                <ModalWrapper type="button" onClick={this.showModal}>
-                  <img
-                    src="https://res.cloudinary.com/cloudwanders/image/upload/v1632100132/2shelter_dy1pwr.png"
-                    alt="Project Showcase - Helter Shelter"
-                    aria-label="Helter Shelter screenshot"
-                  />
-                </ModalWrapper>
-              </Item>
-            </Card>
+      <>
+        <Wrapper>
+          <ProjectContainer id="projects">
+            <Grid>
+              <Card>
+                <Item>
+                  <ModalWrapper
+                    type="button"
+                    onClick={() => this.handleOpenModal('helter')}
+                  >
+                    <img
+                      src="https://res.cloudinary.com/cloudwanders/image/upload/v1632100132/2shelter_dy1pwr.png"
+                      alt="Project Showcase - Helter Shelter"
+                      aria-label="Helter Shelter screenshot"
+                      draggable="false"
+                    />
+                  </ModalWrapper>
 
-            <Card>
-              <Item>
-                <Modal
-                  show={this.state.show}
-                  handleClose={this.hideModal}
-                ></Modal>
-                <ModalWrapper type="button" onClick={this.showModal}>
-                  <img
-                    src="https://res.cloudinary.com/cloudwanders/image/upload/v1632101037/2nebula_p6uq1n.png"
-                    alt="Project Showcase - Nebula"
-                    aria-label="Nebula screenshot"
-                  />
-                </ModalWrapper>
-              </Item>
-            </Card>
+                  <Modal
+                    isOpen={
+                      this.state.handleOpenModal &&
+                      this.state.activeModal === 'helter'
+                    }
+                    contentLabel="Helter Shelter Modal"
+                  >
+                    <div className="content">
+                      <button className="close" onClick={this.handleCloseModal}>
+                        X
+                      </button>
+                      <p>helter skelter shelter test</p>
+                    </div>
+                  </Modal>
+                </Item>
+              </Card>
 
-            <Card>
-              <Item>
-                <Modal
-                  show={this.state.show}
-                  handleClose={this.hideModal}
-                ></Modal>
-                <ModalWrapper type="button" onClick={this.showModal}>
-                  <img
-                    src="https://res.cloudinary.com/cloudwanders/image/upload/v1632100133/2timbuktu_xthppi.png"
-                    alt="Project Showcase - Timbuktu"
-                    aria-label="Timbuktu screenshot"
-                  />
-                </ModalWrapper>
-              </Item>
-            </Card>
+              <Card>
+                <Item>
+                  <ModalWrapper
+                    type="button"
+                    onClick={() => this.handleOpenModal('nebula')}
+                  >
+                    <img
+                      src="https://res.cloudinary.com/cloudwanders/image/upload/v1632101037/2nebula_p6uq1n.png"
+                      alt="Project Showcase - Nebula"
+                      aria-label="Nebula screenshot"
+                      draggable="false"
+                    />
+                  </ModalWrapper>
 
-            <Card>
-              <Item>
-                <Modal
-                  show={this.state.show}
-                  handleClose={this.hideModal}
-                ></Modal>
-                <ModalWrapper type="button" onClick={this.showModal}>
-                  <img
-                    src="https://res.cloudinary.com/cloudwanders/image/upload/v1632100133/2bravebear_gnod6e.png"
-                    alt="Project Showcase - BraveBear"
-                    aria-label="BraveBear screenshot"
-                  />
-                </ModalWrapper>
-              </Item>
-            </Card>
+                  <Modal
+                    isOpen={
+                      this.state.handleOpenModal &&
+                      this.state.activeModal === 'nebula'
+                    }
+                    contentLabel="Nebula Modal"
+                  >
+                    <div className="content">
+                      <button className="close" onClick={this.handleCloseModal}>
+                        X
+                      </button>
+                      <p>nebula test</p>
+                    </div>
+                  </Modal>
+                </Item>
+              </Card>
 
-            <Card>
-              <Item>
-                <Modal
-                  show={this.state.show}
-                  handleClose={this.hideModal}
-                ></Modal>
-                <ModalWrapper type="button" onClick={this.showModal}>
-                  <img
-                    src="https://res.cloudinary.com/cloudwanders/image/upload/v1632706808/2budget_xwrumq.png"
-                    alt="Project Showcase - Budget App"
-                    aria-label="Budget screenshot"
-                  />
-                </ModalWrapper>
-              </Item>
-            </Card>
+              <Card>
+                <Item>
+                  <ModalWrapper
+                    type="button"
+                    onClick={() => this.handleOpenModal('timbuktu')}
+                  >
+                    <img
+                      src="https://res.cloudinary.com/cloudwanders/image/upload/v1632100133/2timbuktu_xthppi.png"
+                      alt="Project Showcase - Timbuktu"
+                      aria-label="Timbuktu screenshot"
+                      draggable="false"
+                    />
+                  </ModalWrapper>
+                  <Modal
+                    isOpen={
+                      this.state.handleOpenModal &&
+                      this.state.activeModal === 'timbuktu'
+                    }
+                    contentLabel="Timbuktu Modal"
+                  >
+                    <div className="content">
+                      <button className="close" onClick={this.handleCloseModal}>
+                        X
+                      </button>
+                      <p>buktu test</p>
+                    </div>
+                  </Modal>
+                </Item>
+              </Card>
 
-            <Card>
-              <Item>
-                <Modal
-                  show={this.state.show}
-                  handleClose={this.hideModal}
-                ></Modal>
-                <ModalWrapper type="button" onClick={this.showModal}>
-                  <img
-                    src="https://res.cloudinary.com/cloudwanders/image/upload/v1632101036/2scraper_ce8h56.png"
-                    alt="Project Showcase - Node Web Scraper"
-                    aria-label="Node Scraper screenshot"
-                  />
-                </ModalWrapper>
-              </Item>
-            </Card>
-          </Grid>
-        </ProjectContainer>
-      </Wrapper>
-    );
+              <Card>
+                <Item>
+                  <ModalWrapper
+                    type="button"
+                    onClick={() => this.handleOpenModal('bravebear')}
+                  >
+                    <img
+                      src="https://res.cloudinary.com/cloudwanders/image/upload/v1632100133/2bravebear_gnod6e.png"
+                      alt="Project Showcase - BraveBear"
+                      aria-label="BraveBear screenshot"
+                      draggable="false"
+                    />
+                  </ModalWrapper>
+
+                  <Modal
+                    isOpen={
+                      this.state.handleOpenModal &&
+                      this.state.activeModal === 'bravebear'
+                    }
+                    contentLabel="BraveBear Modal"
+                  >
+                    <div className="content">
+                      <button className="close" onClick={this.handleCloseModal}>
+                        X
+                      </button>
+                      <p>brave test</p>
+                    </div>
+                  </Modal>
+                </Item>
+              </Card>
+
+              <Card>
+                <Item>
+                  <ModalWrapper
+                    type="button"
+                    onClick={() => this.handleOpenModal('budget')}
+                  >
+                    <img
+                      src="https://res.cloudinary.com/cloudwanders/image/upload/v1632706808/2budget_xwrumq.png"
+                      alt="Project Showcase - Budget App"
+                      aria-label="Budget screenshot"
+                      draggable="false"
+                    />
+                  </ModalWrapper>
+                  <Modal
+                    isOpen={
+                      this.state.handleOpenModal &&
+                      this.state.activeModal === 'budget'
+                    }
+                    contentLabel="Budget Modal"
+                  >
+                    <div className="content">
+                      <button className="close" onClick={this.handleCloseModal}>
+                        X
+                      </button>
+                      <p>budget test</p>
+                    </div>
+                  </Modal>
+                </Item>
+              </Card>
+
+              <Card>
+                <Item>
+                  <ModalWrapper
+                    type="button"
+                    onClick={() => this.handleOpenModal('scraper')}
+                  >
+                    <img
+                      src="https://res.cloudinary.com/cloudwanders/image/upload/v1632101036/2scraper_ce8h56.png"
+                      alt="Project Showcase - Node Web Scraper"
+                      aria-label="Node Scraper screenshot"
+                      draggable="false"
+                    />
+                  </ModalWrapper>
+
+                  <Modal
+                    isOpen={
+                      this.state.handleOpenModal &&
+                      this.state.activeModal === 'scraper'
+                    }
+                    contentLabel="Node Scraper Modal"
+                  >
+                    <div className="content">
+                      <button className="close" onClick={this.handleCloseModal}>
+                        X
+                      </button>
+                      <p>testing scraper</p>
+                    </div>
+                  </Modal>
+                </Item>
+              </Card>
+            </Grid>
+          </ProjectContainer>
+        </Wrapper>
+      </>
+    )
   }
-}
+};
--- src/components/sections/Skills/index.jsx
+++ src/components/sections/Skills/index.jsx
@@ -60,5 +60,5 @@ export const Skills = () => {
         />
       </Grid>
     </SkillsContainer>
-  );
+  )
 };
