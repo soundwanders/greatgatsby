2dbfdd424bd01d73f8e46c421ccd5c87f4bbf629
soundwanders <soundwanders@gmail.com>
2021-10-22 12:46:26 -0400

    Viewport meta tag

--- src/components/common/Seo/index.js
+++ src/components/common/Seo/index.js
@@ -51,6 +51,7 @@ export const Seo = ({
         name="image"
         content="https://res.cloudinary.com/cloudwanders/image/upload/v1632713646/thumbnail_bpik7e.png"
       />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
       <meta property="og:url" content={`${url}${location}/?ref=jcoletta.com`} />
       <meta property="og:type" content="website" />
