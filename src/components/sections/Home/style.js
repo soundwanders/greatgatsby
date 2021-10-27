fb5f250e43052912b3c36a50638bcf365de27374
soundwanders <soundwanders@gmail.com>
2021-10-27 10:27:22 -0400

    Update style.js

--- src/components/sections/Home/style.js
+++ src/components/sections/Home/style.js
@@ -18,7 +18,7 @@ export const HomeContainer = styled.div`
 
   @media (max-width: 940px) {
     background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='600 20 1600 900'%3E%3Cpolygon fill='%23c2c1dc' points='957 450 539 900 1396 900'/%3E%3Cpolygon fill='%23ced0dc' points='957 450 872.9 900 1396 900'/%3E%3Cpolygon fill='%23cdcce2' points='-60 900 398 662 816 900'/%3E%3Cpolygon fill='%23cdcfdc' points='337 900 398 662 816 900'/%3E%3Cpolygon fill='%23d8d7e7' points='1203 546 1552 900 876 900'/%3E%3Cpolygon fill='%23cbcddc' points='1203 546 1552 900 1162 900'/%3E%3Cpolygon fill='%23e2e2ed' points='641 695 886 900 367 900'/%3E%3Cpolygon fill='%23caccdc' points='587 900 641 695 886 900'/%3E%3Cpolygon fill='%23ededf2' points='1710 900 1401 632 1096 900'/%3E%3Cpolygon fill='%23c9cadc' points='1710 900 1401 632 1365 900'/%3E%3Cpolygon fill='%23f8f8f8' points='1210 900 971 687 725 900'/%3E%3Cpolygon fill='%23c8c9dc' points='943 900 1210 900 971 687'/%3E%3C/svg%3E");
-  }
+ }
   @media (max-width: 600px) {
     background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='920 -80 1600 900'%3E%3Cpolygon fill='%23c2c1dc' points='957 450 539 900 1396 900'/%3E%3Cpolygon fill='%23ced0dc' points='957 450 872.9 900 1396 900'/%3E%3Cpolygon fill='%23cdcce2' points='-60 900 398 662 816 900'/%3E%3Cpolygon fill='%23cdcfdc' points='337 900 398 662 816 900'/%3E%3Cpolygon fill='%23d8d7e7' points='1203 546 1552 900 876 900'/%3E%3Cpolygon fill='%23cbcddc' points='1203 546 1552 900 1162 900'/%3E%3Cpolygon fill='%23e2e2ed' points='641 695 886 900 367 900'/%3E%3Cpolygon fill='%23caccdc' points='587 900 641 695 886 900'/%3E%3Cpolygon fill='%23ededf2' points='1710 900 1401 632 1096 900'/%3E%3Cpolygon fill='%23c9cadc' points='1710 900 1401 632 1365 900'/%3E%3Cpolygon fill='%23f8f8f8' points='1210 900 971 687 725 900'/%3E%3Cpolygon fill='%23c8c9dc' points='943 900 1210 900 971 687'/%3E%3C/svg%3E");
     height: 92vh;
@@ -58,7 +58,7 @@ export const Subtitle = styled.h1`
   font-family: 'LuloCleanW01-OneBold', 'Roboto', 'Ubuntu', sans-serif;
   text-rendering: optimizeSpeed;
   @media (max-width: 600px) {
-    font-size: 2vmax;
+    font-size: 1.9vmax;
     padding: 0 0 2rem 0;
   }
 `;
